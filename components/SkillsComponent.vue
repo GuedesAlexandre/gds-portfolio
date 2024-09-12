d959c2fff416346ed5dd7e7248d2ef3c2bc37c08
GuedesAlexandre <alexandreguedes77600@gmail.com>
2024-09-12 19:07:35 +0200

    feat : fin du composants skills

--- components/SkillsComponent.vue
+++ components/SkillsComponent.vue
@@ -2,46 +2,83 @@
   <div class="flex justify-center flex-col items-center mt-40" id="about">
     <TitleComponent title="Mes compétences" subtitle="Ma stack" />
   </div>
-  <div class="flex flex-row justify-center items-center gap-4 mt-2">
-   
+  <div
+    class="flex lg:flex-row flex-col justify-center items-center gap-10 mt-8 lg:ml-8"
+  >
     <SkillsTableComponent :skills="front" />
-    <SkillsTableComponent :skills="front" />
-    <SkillsTableComponent :skills="front" />
-
+    <SkillsTableComponent :skills="back" />
+    <SkillsTableComponent :skills="other" />
   </div>
-
 </template>
 
 <script lang="ts" setup>
-import type { Skill } from '~/models/Skill';
-
+import type { Skill } from "~/models/Skill";
 
 const front = ref<Skill[]>([
   {
-    name: "Angular",
-    icon: "../assets/icons/front/Angularfront.svg",
-  },
-  {
     name: "TypeScript",
-    icon: "../assets/icons/front/TypeScriptfront.svg",
+    icon: new URL("@/assets/icons/front/TypeScriptfront.svg", import.meta.url)
+      .href,
   },
   {
     name: "React",
-    icon: "../assets/icons/front/Reactfront.svg",
+    icon: new URL("@/assets/icons/front/Reactfront.svg", import.meta.url).href,
   },
   {
     name: "Nuxt",
-    icon: "../assets/icons/front/Nuxtfront.svg",
+    icon: new URL("@/assets/icons/front/Nuxtfront.svg", import.meta.url).href,
   },
   {
     name: "Tailwind",
-    icon: "../assets/icons/front/Tailwindfront.svg",
+    icon: new URL("@/assets/icons/front/Tailwindfront.svg", import.meta.url)
+      .href,
   },
   {
-    name:"Sass",
-    icon:"../assets/icons/front/Sassfront.svg"
-  }
+    name: "Sass",
+    icon: new URL("@/assets/icons/front/Sassfront.svg", import.meta.url).href,
+  },
+]);
+
+const back = ref<Skill[]>([
+  {
+    name: "Spring",
+    icon: new URL("@/assets/icons/back/Spring.svg", import.meta.url).href,
+  },
+  {
+    name: "Docker",
+    icon: new URL("@/assets/icons/back/Docker.svg", import.meta.url).href,
+  },
+  {
+    name: "Laravel",
+    icon: new URL("@/assets/icons/back/Laravel.svg", import.meta.url).href,
+  },
+  {
+    name: "PostGreSql",
+    icon: new URL("@/assets/icons/back/PostGre.svg", import.meta.url).href,
+  },
+  {
+    name: "Kubernetes",
+    icon: new URL("@/assets/icons/back/Kubernetes.svg", import.meta.url).href,
+  },
+]);
+
+const other = ref<Skill[]>([
+  {
+    name: "Arc",
+    icon: new URL("@/assets/icons/other/Arc.svg", import.meta.url).href,
+  },
+  {
+    name: "Figma",
+    icon: new URL("@/assets/icons/other/Figma.svg", import.meta.url).href,
+  },
+  {
+    name: "Git",
+    icon: new URL("@/assets/icons/other/Git.svg", import.meta.url).href,
+  },
+  {
+    name: "StackOverflow",
+    icon: new URL("@/assets/icons/other/StackOverflow.svg", import.meta.url)
+      .href,
+  },
 ]);
 </script>
-
-
--- components/SkillsTableComponent.vue
+++ components/SkillsTableComponent.vue
@@ -1,17 +1,20 @@
 <template>
-  <div class="flex flex-col align-center"> 
+  <div class="flex flex-col align-center gap-4"> 
     <div v-for="skill in props.skills" :key="skill.name" class="flex items-center gap-2">
-      <img class="w-6 h-6" :src="skill.icon"/>
-      <h3 class="text-lg font-bold">{{ skill.name }}</h3>
+      <img class="w-6 h-6" :src="skill.icon" @error="onImageError(skill.icon)"/>
+      <h3 class="text-lg  text-Text">{{ skill.name }}</h3>
     </div>
   </div>
 </template>
 
 <script setup lang="ts" >
 import type { Skill } from '~/models/Skill';
-
 //define props with type skills
 const props = defineProps<{
   skills: Skill[]
 }>()
+
+const onImageError = (src: string) => {
+  console.error(`Image not found: ${src}`);
+}
 </script>
