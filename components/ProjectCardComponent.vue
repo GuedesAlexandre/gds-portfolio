abd9e985a49c06d10f84779e6ac03c313d3b0f3a
GuedesAlexandre <alexandreguedes77600@gmail.com>
2024-09-15 11:40:33 +0200

    eslint fix

--- components/ProjectCardComponent.vue
+++ components/ProjectCardComponent.vue
@@ -2,12 +2,12 @@
   <div class="flex flex-col">
     <div 
       class="bg-cover bg-center lg:w-96 lg:h-60 w-72 h-60  rounded-md" 
-      :style="{ backgroundImage: `url(${project.img})` }"
+      :style="{ backgroundImage: `url(${props.project.img})` }"
       aria-label="Project Image"
     >
     </div>
     <div class="flex flex-col lg:flex-row lg:justify-between items-center mt-3">
-      <h3 class="lg:text-lg text-md font-semibold text-Text">{{ project.name }}</h3>
+      <h3 class="lg:text-lg text-md font-semibold text-Text">{{props.project.name }}</h3>
       <button class="text-2xl font-bold p-2 rounded-full bg-Text hover:bg-Accent hover:ring-1 cursor-pointer transition-colors "><img src="../assets/icons/other/Arrow.svg" ></button>
     </div>
   </div>
